3becbad5edb5dc434e4d168233b6c3d21a109f92
Sterling Radcliff <radcliff@riseup.net>
2015-07-30 14:51:42 -0700

    add tonight#index (archive) for ifttt protocol

--- app/controllers/api/v1/tonight_controller.rb
+++ app/controllers/api/v1/tonight_controller.rb
@@ -6,6 +6,14 @@ module Api
         @tonight = Tonight.find_or_create
       end
 
+      def index
+        count = params[:count].to_i
+        return @archive = [] if count == 0
+
+        keys = $redis.lrange('tonight:index', 0, count - 1)  # archive is a stack, pull from left
+        @archive = keys.map { |key| $redis.hgetall(key) }  # map over keys -> array of attr hashes
+      end
+
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'tonight', to: 'tonight#show'
+      get 'tonight/archive', to: 'tonight#index'
     end
   end
 
